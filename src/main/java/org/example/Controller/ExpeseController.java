4b37d308870920c30dd57a1d862696ec7e94a936
ricardocollado <ricardocolladorothschild@gmail.com>
2024-03-16 02:57:36 -0400

    add a missing dependency for web.api

--- src/main/java/org/example/Controller/ExpeseController.java
+++ src/main/java/org/example/Controller/ExpeseController.java
@@ -1,4 +1,6 @@
 package org.example.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 public class ExpeseController {
 }
